40b05126411a81e80be47edc0f40338f9c9e56d1
Dan Murphy <dmurphy79@gmail.com>
2020-08-31 18:06:13 -0500

    adds type to App component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,8 +1,10 @@
-import * as React from 'react';
+import * as React from "react";
 
-export const App = () => (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <h1>Lucid</h1>
-        <h2>Welcome to UI Team code assessment!</h2>
-    </div>
+export const App: React.FC = () => (
+  <div
+    style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+  >
+    <h1>Lucid</h1>
+    <h2>Welcome to UI Team code assessment!</h2>
+  </div>
 );
