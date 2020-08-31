c3f31ed9fbc0ed0641d8efadcc2eadf371fed5d8
Dan Murphy <dmurphy79@gmail.com>
2020-08-31 18:23:16 -0500

    imports graphql, express-graphql and inits temporary middleware

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,40 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
-const data = require('./data.json');
+const data = require("./data.json");
 
 // create server
 const server = express();
 const port = 4000;
 
+// set up GraphQL schema
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// TODO: Remove test function
+const root = { hello: () => "Hello world!" };
+
+// Use graphqlhttp middleware
+server.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
 // GET question endpoint
 server.get("/api/questions", cors(), (req, res) => {
-    res.json(data);
+  res.json(data);
 });
 
 // starting server
 server.listen(port, () => {
-    console.log(`Server listening at ${port}`);
+  console.log(`Server listening at ${port}`);
 });
