b26150898e467428f4932ccb2e7223324ca3a8b9
Viktor Suber <viktor.suber@gmail.com>
2020-09-04 20:46:00 -0700

    pass data into questions component

--- src/client/Quiz/components/Questions.tsx
+++ src/client/Quiz/components/Questions.tsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export const Questions = (questions: any) => {
+export const Questions: React.FC <{questions: object}> = ({questions}) => {
+    // console.log('QUESTIONS', questions);
+    return (
+        <div>
+            test
+        </div>
+    );
 
-    questions && questions.map ( (question: any) => {
-        return (
-            <div>
-                {question}
-            </div>
-            );
-    } )
+    // questions && questions.map ( (question: any) => {
+    //     return (
+    //         <div>
+    //             {question}
+    //         </div>
+    //         );
+    // } );
 
 };
