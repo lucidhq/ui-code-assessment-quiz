c79017d719215ad495cd7594649e6878789c83d8
Viktor Suber <viktor.suber@gmail.com>
2020-09-05 13:47:40 -0700

    update file structure

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import './styles.css'
-import { Quiz } from './Quiz/Quiz';
+import './styles/styles.css';
+import { Quiz } from './modules/Quiz/Quiz';
 
 export const App = () => (
     <div className="container">
