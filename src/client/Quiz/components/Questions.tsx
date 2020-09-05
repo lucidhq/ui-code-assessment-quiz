d9b98034d04536c0194ae2382a87b6128f1054b1
Viktor Suber <viktor.suber@gmail.com>
2020-09-05 13:35:38 -0700

    update

--- src/client/Quiz/components/Questions.tsx
+++ src/client/Quiz/components/Questions.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Question } from './Question/Question';
 
 export const Questions: React.FC <{questions: object}> = ({questions}) => {
     // console.log('QUESTIONS', questions);
+
+    // create question state
+
+    // const [question, setQuestionData] = useState(questions[Math.floor(Math.random() * questions.length)]);
     
     return (
         <div>
