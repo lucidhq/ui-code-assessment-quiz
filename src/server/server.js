352c6eede52a87b4e0f0a24f6647967b3f316310
Viktor Suber <viktor.suber@gmail.com>
2020-09-07 12:46:11 -0700

    split questions of each type into sets

--- src/server/server.js
+++ src/server/server.js
@@ -15,6 +15,20 @@ const shuffle = (data) => {
   return data;
 };
 
+const splitArray = (array, arrayLength) => {
+    const newArray = [];
+
+    let i = 0;
+
+    while (i < array.length) {
+      newArray.push(array.slice(i, arrayLength + i));
+      i += arrayLength;
+    }
+
+    return newArray;
+
+};
+
 const sortDataByType = (data) => {
     return data.reduce((seed, current) => {
         if (!seed[current.type]) {
@@ -45,19 +59,27 @@ const randomizeMultiple = (data) => {
     return newDataSet;
 };
 
-// const sortDataSets = (data) => {
-//     let sets = [];
+const sortDataSets = (data) => {
+    let sets = [];
 
-//     data.text.forEach(question => {
-//         sets.push([question]);
-//     });
+    data.text.forEach(question => {
+        sets.push([question]);
+    });
 
-//     const newBooleanSet = data.boolean.slice(2);
+    const newBooleanSet = splitArray(data.boolean.slice(2), 2);
 
-//     return {
-//         results: sets
-//     };
-// };
+    const newMultipleSet = splitArray(data.multiple, 9);
+
+    // console.log('NEW TEXT SET', sets);
+    // console.log('NEW BOOLEAN SET', newBooleanSet);
+    // console.log('NEW MULTIPLE SET', newMultipleSet);
+
+    return {
+            text: sets,
+            boolean: newBooleanSet,
+            multiple: newMultipleSet
+    };
+};
 
 // GET question endpoint
 server.get("/api/questions", cors(), (req, res) => {
@@ -68,8 +90,8 @@ server.get("/api/questions", cors(), (req, res) => {
     // Consolidate all multiple choice questions into one array and randomize placement of correct answer
     const sortedDataModifiedMultiple = randomizeMultiple(sortedData);
     // Separate into 4 sets with 9 multiple choice questions, 2 boolean questions, 1 text question
-    // const sortedDataBySet = sortDataSets(ssortedDataModifiedMultiple);
-    res.json(sortedDataModifiedMultiple);
+    const sortedDataBySet = sortDataSets(sortedDataModifiedMultiple);
+    res.json(sortedDataBySet);
 });
 
 // starting server
