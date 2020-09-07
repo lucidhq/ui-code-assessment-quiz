bbb190d063be050a909aa3299450640fef3ed0da
Viktor Suber <viktor.suber@gmail.com>
2020-09-07 05:10:34 -0700

    submit button

--- src/client/components/Multiple/Multiple.tsx
+++ src/client/components/Multiple/Multiple.tsx
@@ -10,10 +10,14 @@ export const Multiple = (question: object) => {
 
   const answers = ["Rad Mobile", "Sonic The Hedgehog", "Super Mario 64", "Mega Man"];
 
+  const onSubmit = (data: any) => {
+    console.log('DATA', data);
+  };
+
   return (
     <div className="multiple">
       <div className="question">{q}</div>
-      <form className="question-list">
+      <form className="question-list" onSubmit={onSubmit}>
         <label className="option">
         <input className="radio" type="radio" value="0" name="answer" />
         Rad Mobile
@@ -28,8 +32,8 @@ export const Multiple = (question: object) => {
         <label className="option">
         <input className="radio"  type="radio" value="3" name="answer"/>
         Mega Man</label>
+        <button className="button" type="submit"><span className="button-text">Next</span></button>
       </form>
-        <button className="button"><span className="button-text">Next</span></button>
     </div>
   );
 };
