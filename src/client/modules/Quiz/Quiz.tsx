4e301f8d3570e029c9c6d505c12669ffac46d08e
Viktor Suber <viktor.suber@gmail.com>
2020-09-07 18:57:52 -0700

    code clean up

--- src/client/modules/Quiz/Quiz.tsx
+++ src/client/modules/Quiz/Quiz.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Question } from '../Question/Question';
 import { Multiple } from '../../components/Multiple/Multiple';
 import { Boolean } from '../../components/Boolean/Boolean';
@@ -8,7 +7,6 @@ import { TextQuestion } from '../../components/TextQuestion/TextQuestion';
 
 export const Quiz = () => {
 
-    const [data, setData] = useState([]);
     const [questions, setQuestions] = useState([]);
     const [questionSet, setQuestionSet] = useState([]);
     const [currentSetIndex, setCurrentSetIndex] = useState(0);
@@ -17,29 +15,16 @@ export const Quiz = () => {
     const [summaryData, setSummaryData] = useState({correct: 0, incorrect: 0});
 
     // Get all questions
-    // useEffect(() => {
-    //   axios.get('http://localhost:4000/api/questions')
-    //     .then( res => {
-    //         setQuestions(res.data);
-           
-    //     })
-    //     .catch( err => {
-    //         console.error('ERROR', err);
-    //     });
-    // });
-
     useEffect(() => {
         fetch('http://localhost:4000/api/questions')
         .then(res => res.json())
-        .then(setData)
+        .then(setQuestions)
         .catch(console.log);
-
-    //    setCurrentQuestion(questionSet[currentQuestionIndex]);
-    }, [questions]);
+    }, []);
 
     return (
     <div>
-        {JSON.stringify(data)}
+        {JSON.stringify(questions)}
         {/* {questionSet[currentQuestionIndex]['type'] === 'multiple' ? <Multiple /> : null }
         {questionSet[currentQuestionIndex]['type'] === 'boolean' ? <Boolean /> : null }
         {questionSet[currentQuestionIndex]['type'] === 'text' ? <TextQuestion /> : null } */}
