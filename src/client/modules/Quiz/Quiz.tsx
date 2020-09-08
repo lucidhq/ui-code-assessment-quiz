eb0cebaed7f85b3d20d62bc1858d7683c8f668c2
Viktor Suber <viktor.suber@gmail.com>
2020-09-08 10:53:06 -0700

    update text question component

--- src/client/modules/Quiz/Quiz.tsx
+++ src/client/modules/Quiz/Quiz.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Questions } from '../Questions/Questions';
 import { Multiple } from '../../components/Multiple/Multiple';
 import { Boolean } from '../../components/Boolean/Boolean';
+import { TextQuestion } from '../../components/TextQuestion/TextQuestion';
 
 interface Props {
     bool: Array<any>,
@@ -14,9 +15,8 @@ export const Quiz: React.FC<Props> = (props) => {
     return (
     <div>
         {/* <Multiple question={props.multiple[0] && props.multiple[0].question} answers={props.multiple[0] && props.multiple[0].answers} correctAnswer={props.multiple[0] &&  props.multiple[0].correctAnswer}/> */}
-        <Boolean question={props.bool[0] && props.bool[0].question} correctAnswer={props.bool[0] && props.bool[0].correctAnswer} />
-        {/* 
-        {JSON.stringify(props.text[0])} */}
+        {/* <Boolean question={props.bool[0] && props.bool[0].question} correctAnswer={props.bool[0] && props.bool[0].correctAnswer} /> */}
+        <TextQuestion question={props.text[0] && props.text[0].question} correctAnswer={props.text[0] && props.text[0].correctAnswer} />
     </div>
     );
 
