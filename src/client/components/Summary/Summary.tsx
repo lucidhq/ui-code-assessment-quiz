d58ecdd2bed16bf318197b75b62f1c761c245fda
Viktor Suber <viktor.suber@gmail.com>
2020-09-09 15:32:33 -0700

    calculate and display questions answered, final score

--- src/client/components/Summary/Summary.tsx
+++ src/client/components/Summary/Summary.tsx
@@ -11,11 +11,18 @@ export const Summary: React.FC<Summary> = (props: any) => {
 
   const [correct, setCorrect] = useState(0);
   const [wrong, setWrong] = useState(0);
+  const [questionsAnswered, setQuestionsAnswered] = useState(0);
+  const [finalScore, setFinalScore] = useState(0);
 
   useEffect(() => {
     setCorrect(props.scoreData.correct);
     setWrong(props.scoreData.wrong);
   }, [props]);
+
+  useEffect(() => {
+    setQuestionsAnswered(correct + wrong);
+    setFinalScore(Math.round(correct / wrong) * 100);
+  }, [correct, wrong]);
 
   return (
     <div className="summary">
@@ -23,8 +30,8 @@ export const Summary: React.FC<Summary> = (props: any) => {
         <ul className="summary-list">
           <li>Correct: <b>{correct}</b></li>
           <li>Wrong: <b>{wrong}</b></li>
-          <li>Questions Answered: <b>3</b></li>
-          <li>Final Score: <b>70%</b></li>
+          <li>Questions Answered: <b>{questionsAnswered}</b></li>
+          <li>Final Score: <b>{finalScore}%</b></li>
         </ul>
       </div>
   );
