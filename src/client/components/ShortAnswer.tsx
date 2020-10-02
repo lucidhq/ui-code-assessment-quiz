c8ac8666066c49b0557f4fdff821240a18142b23
Chase Kallemeyn <chase.kallemeyn@gmail.com>
2020-10-01 22:26:41 -0500

    [chore] cleans up ShortAnswer component

--- src/client/components/ShortAnswer.tsx
+++ src/client/components/ShortAnswer.tsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react'
 import { Input } from 'semantic-ui-react'
 import QuestionContext from "../contexts/QuestionContext";
 
-// { currentAnswer, handleChange }: any
 const ShortAnswer = () => {
   const { currentAnswer, handleChange }: any = useContext(QuestionContext);
 
